784a1f62e42064783de4cac9c17c262aef2da657
EdiisonTene19 <120532454+EdiisonTene19@users.noreply.github.com>
2023-01-21 21:24:37 -0500

    usuario

---
+++ usuario.php
@@ -0,0 +1,88 @@
+<?php
+include "config.php";
+include "utils.php";
+
+$dbConn =  connect($db);
+
+// SELECT 
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['codigo']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * usuario  where idUsuario=:idUsuario");
+      $sql->bindValue(':codigo', $_GET['codigo']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+
+	  else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM usuario");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
+}
+
+//INSERTAR
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $input = $_POST;
+    $sql = "INSERT INTO usuario
+          (idUsuario, idTipoUsuario, nombreUsuario, apellidoUsuario, correoUsuario, usuario, passwordUsuario, estadoUsuario)
+          VALUES
+          (:idUsuario, :idTipoUsuario, :nombreUsuario, :apellidoUsuario, :correoUsuario, :usuario, :passwordUsuario, :estadoUsuario)";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+
+    $postCodigo = $dbConn->lastInsertId();
+    if($postCodigo)
+    {
+      $input['idUsuario'] = $postCodigo;
+      header("HTTP/1.1 200 OK");
+      echo json_encode($input);
+      exit();
+	 }
+}
+//Eliminar
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	$codigo = $_GET['idUsuario'];
+  $statement = $dbConn->prepare("DELETE FROM  usuario where 	idUsuario=:idUsuario");
+  $statement->bindValue('idUsuario', $codigo);
+  $statement->execute();
+	header("HTTP/1.1 200 OK");
+	exit();
+}
+
+//Actualizar
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $postCodigo = $input['idUsuario'];
+    $fields = getParams($input);
+
+    $sql = "
+          UPDATE usuario
+          SET $fields
+          WHERE idUsuario='$postCodigo'
+           ";
+
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+
+
+?>
