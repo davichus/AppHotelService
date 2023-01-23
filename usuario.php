09c524e30c2f15a1e2dcb97b670dc4e7ab836ef2
EdiisonTene19 <120532454+EdiisonTene19@users.noreply.github.com>
2023-01-23 15:41:40 -0500

    modificacion

--- usuario.php
+++ usuario.php
@@ -34,9 +34,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $input = $_POST;
     $sql = "INSERT INTO usuario
-          (idUsuario, idTipoUsuario, nombreUsuario, apellidoUsuario, correoUsuario, usuario, contraseñaUsuario, estadoUsuario)
+          (idUsuario, idTipoUsuario, nombreUsuario, apellidoUsuario, correoUsuario, usuario, passwordUsuario, estadoUsuario)
           VALUES
-          (NULL, :idTipoUsuario, :nombreUsuario, :apellidoUsuario, :correoUsuario, :usuario, :contraseñaUsuario, 'A')";
+          (NULL, '1', :nombreUsuario, :apellidoUsuario, :correoUsuario, :usuario, :passwordUsuario, 'A')";
     $statement = $dbConn->prepare($sql);
     bindAllValues($statement, $input);
     $statement->execute();
