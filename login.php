a71729a9fcc9c80c6ae77fb45e215ca04f0758dc
EdiisonTene19 <120532454+EdiisonTene19@users.noreply.github.com>
2023-01-26 11:39:25 -0500

    login

---
+++ login.php
@@ -0,0 +1,34 @@
+<?php
+include "config.php";
+include "utils.php";
+
+$dbConn =  connect($db);
+
+// SELECT 
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET["correoUsuario"]) && isset($_GET["passwordUsuario"]))
+    {
+        $usuario=$_GET['correoUsuario'];
+        $pass=$_GET['passwordUsuario'];
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT correoUsuario, passwordUsuario from usuario  where usuario= '{$usuario}' AND pass = '{$pass}'");
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+
+	  else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM Habitacion");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
+}
+
+
+?>
