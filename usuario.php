0f9926f017d4b987b710e1de6945d6fcec625391
EdiisonTene19 <120532454+EdiisonTene19@users.noreply.github.com>
2023-01-26 11:32:37 -0500

    modificacion

--- usuario.php
+++ usuario.php
@@ -7,11 +7,11 @@ $dbConn =  connect($db);
 // SELECT 
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
-    if (isset($_GET['codigo']))
+    if (isset($_GET['idUsuario']))
     {
       //Mostrar un post
       $sql = $dbConn->prepare("SELECT * from usuario  where idUsuario=:idUsuario");
-      $sql->bindValue(':codigo', $_GET['codigo']);
+      $sql->bindValue(':idUsuario', $_GET['idUsuario']);
       $sql->execute();
       header("HTTP/1.1 200 OK");
       echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
