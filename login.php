9d337c8c0b575e6747dfb53a8e25ec09d5f10721
EdiisonTene19 <120532454+EdiisonTene19@users.noreply.github.com>
2023-01-26 12:58:22 -0500

    Update login.php

--- login.php
+++ login.php
@@ -7,12 +7,12 @@ $dbConn =  connect($db);
 // SELECT 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-    if (isset($_GET['correoUsuario']) && isset($_GET["passwordUsuario"]))
+    if (isset($_POST['correoUsuario']) && isset($_POST["passwordUsuario"]))
     {
       //Mostrar un post
       $sql = $dbConn->prepare("SELECT * from usuario  where correoUsuario=:correoUsuario and passwordUsuario=:passwordUsuario");
-      $sql->bindValue(':correoUsuario', $_GET['correoUsuario']);
-      $sql->bindValue(':passwordUsuario', $_GET['passwordUsuario']);
+      $sql->bindValue(':correoUsuario', $_POST['correoUsuario']);
+      $sql->bindValue(':passwordUsuario', $_POST['passwordUsuario']);
       $sql->execute();
       header("HTTP/1.1 200 OK");
       echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
