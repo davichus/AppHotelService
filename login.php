410f0ac0e0bbcb253b405847a8aba62bbff789ec
EdiisonTene19 <120532454+EdiisonTene19@users.noreply.github.com>
2023-01-26 12:15:16 -0500

    Update login.php

--- login.php
+++ login.php
@@ -3,42 +3,30 @@ include "config.php";
 include "utils.php";
 
 $dbConn =  connect($db);
-if(isset($_GET["correoUsuario"]) && isset($_GET["passwordUsuario"])){
-    $usuario=$_GET['correoUsuario'];
-    $pass=$_GET['passwordUsuario'];
-    
-    $conexion=mysqli_connect($hostname_localhost,$usuarioname_localhost,$password_localhost,$database_localhost);
-    
-    $consulta="SELECT correoUsuario,passwordUsuario FROM usuario WHERE correoUsuario= '{$usuario}' AND passwordUsuario = '{$pass}'";
-    $resultado=mysqli_query($dbConn,$consulta);
 
-    if($consulta){
-    
-        if($registro=mysqli_fetch_array($resultado)){
-            $json['datos'][]=$registro;
-            
-        }
-        mysqli_close($conexion);
-        echo json_encode($json);
-    }
+// SELECT 
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['correoUsuario']) && isset($_GET["passwordUsuario"]))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * from usuario  where correoUsuario=:correoUsuario and passwordUsuario=:passwordUsuario");
+      $sql->bindValue(':idUsuario', $_GET['idUsuario']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
 
-
-
-    else{
-        $results["usuario"]='No Registra';
-        $results["pass"]='No Registra';
-        $results["nombre"]='No Registra';
-        $json['datos']['']=$results;
-        echo json_encode($json);
-    }
-    
+	  else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM usuario");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
 }
-else{
-        $results["correoUsuario"]='No Retorna';
-        $results["passwordUsuario"]='No Retorna';
-        $json['usuario'][]=$results;
-        echo json_encode($json);
-    }
-
 
 ?>
