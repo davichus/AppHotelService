376cb98fb2eae2e50526583d4fc3ee2669896ed2
EdiisonTene19 <120532454+EdiisonTene19@users.noreply.github.com>
2023-01-26 16:38:35 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,37 @@
 <?php
-include "config.php";
-include "utils.php";
+#include "config.php";
+#include "utils.php";
 
-$dbConn =  connect($db);
+#$dbConn =  connect($db);
 
 // SELECT 
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    if (isset($_POST['correoUsuario']) && isset($_POST["passwordUsuario"]))
-    {
-      $sql = $dbConn->prepare("SELECT * from usuario  where correoUsuario=:correoUsuario and passwordUsuario=:passwordUsuario");
-      $sql->bindValue(':correoUsuario', $_POST['correoUsuario']);
-      $sql->bindValue(':passwordUsuario', $_POST['passwordUsuario']);
-      $sql->execute();
-      header("HTTP/1.1 200 OK");
-      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
-      exit();
-	  }
+#if ($_SERVER['REQUEST_METHOD'] == 'POST')
+#{
+ #   if (isset($_POST['correoUsuario']) && isset($_POST["passwordUsuario"]))
+  #  {
+   #   $sql = $dbConn->prepare("SELECT * from usuario  where correoUsuario=:correoUsuario and passwordUsuario=:passwordUsuario");
+    #  $sql->bindValue(':correoUsuario', $_POST['correoUsuario']);
+    #  $sql->bindValue(':passwordUsuario', $_POST['passwordUsuario']);
+     # $sql->execute();
+     # header("HTTP/1.1 200 OK");
+     # echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      #exit();
+	  #}
 
 
-}
+#}
+
+//update the above with your database credentials
+header("Content-Type:application/json");
+$con=mysqli_connect("localhost","root","","AppHotelDB");
+$email = $_POST["correoUsuario"];
+$password = $_POST["passwordUsuario"];
+if(mysqli_num_rows(mysqli_query($con,"SELECT * FROM usuario WHERE correoUsuario='$email' AND passwordUsuario='$password'"))> 0){
+
+    $json = array("status" => 200,'message' => "Success");
+    
+    }else{
+        $json = array("status" => 300,'message' => "Error");
+    }
 
 ?>
